11774c2235e36d936c616defa9f90a3f2152becc
Kyuvi <Kyuvi@proton.me>
2023-01-30 20:54:33 +0200

    Added validation functions to clj.csvs

--- src/hctb/clj/csvs.clj
+++ src/hctb/clj/csvs.clj
@@ -1,2 +1,45 @@
 (ns hctb.clj.csvs
- (:require [clojure.data.csv :as csv]) )
+  (:require [clojure.data.csv :as csv]
+            [java-time :as jt]
+            [hctb.clj.utils :as util]) )
+
+(defn validate-latitude
+  [x]
+  (if-let [numa (util/string->double x)]
+    (when (<= -180 numa 180) numa)))
+
+(defn validate-logtitude
+  [x]
+  (if-let [numa (util/string->double x)]
+    (when (<= -90 x 90) numa) ))
+
+(defn validate-timestamp
+  [x]
+  (let [stamp (try util/parse-datetime x
+               (catch Exception e false))]
+    stamp
+    )
+  )
+
+(defn valid-time-sequence?
+  [start-time stop-time]
+  (jt/after? stop-time start-time)
+  )
+
+(defn validate-station-id
+  [x]
+  (if-let [numa (util/string->long x nil)]
+    (when (pos? numa) numa) ))
+
+(defn greater-than-ten
+  [x]
+  (if-let [numa (util/string->long x nil)]
+    (when  (> numa 10) numa) ))
+
+
+
+;; (defn process-journey-row
+;;   )
+
+;; (defn process-station-row
+;;   )
