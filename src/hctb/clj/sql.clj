3bc57354061f893cbe8940aa8e2ff5fe3e5834f8
Kyuvi <Kyuvi@proton.me>
2023-01-31 18:07:29 +0200

    create table functions (clj/sql.clj)

--- src/hctb/clj/sql.clj
+++ src/hctb/clj/sql.clj
@@ -9,4 +9,31 @@
                     "integer" "integer"])
 
 (def station-types ["integer" "integer"  "text"  "text"  "text"  "text"  "text"
-                    "text" "integer" "integer" "integer"])
+                    "text" "text" "text" "integer"
+                    "double_precision" "double_precision"])
+
+(defn build-sql-table-commands
+  ([table-name header-seq]
+   (define-sql-table table-name header-seq ""))
+  ([table-name header-seq ending-string]
+   (let [journey-prep (= (count header-seq) 8)
+         ;; table-name (if journey-prep file-name "Hsl_bike_stations")
+         table-types (if journey-prep journey-types station-types)
+         ;; col-defs (apply
+         ;;           str (interpose ",\n" (map (fn [col t-type]
+         ;;                                       (format"\t%s %s" col t-type ))
+         ;;                                     header-seq table-types)))
+         col-defs (->> (map (fn [col t-type] (format"\t%s %s" col t-type ))
+                            header-seq table-types)
+                       (interpose ",\n")
+                       (apply str)  )
+         ]
+     (vec
+      (format "DROP TABLE IF EXISTS %s;" table-name)
+      (format "CREATE TABLE %s (\n%s %s\n);"
+              table-name col-defs ending-string))
+     )))
+
+(defn make-sql-table
+  [db table-name header-seq]
+  (sql/db-do-commands db (build-sql-table-commands table-name header-seq)))
