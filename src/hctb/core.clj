cf4ecfc0be3ee31f2f32148dc202b9a603b7bca7
Kyuvi <Kyuvi@proton.me>
2023-02-01 20:24:18 +0200

    defined -main funcion in core.clj
    Also added requied namespaces

--- src/hctb/core.clj
+++ src/hctb/core.clj
@@ -1,7 +1,18 @@
 (ns hctb.core
-  (:gen-class))
+  (:gen-class)
+  (:require [hctb.clj.sql :as hs]))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  ;; "I don't do a whole lot ... yet."
+  ;; [& args]
+  ([] (-main hs/default-db ))
+  ([db]
+   (let [csvdir (System/getenv "CSVDIR")]
+     (when-not csvdir
+       (throw
+        (Exception.
+         "No CSVDIR, please specify a directory as the CSVDIR environment variable.")))
+     (when-not (hs/db-connection? db)
+      (throw (Exception. (str "Unable to connect to DB:" db))))
+     (hs/load-csvs-to-db db csvdir)
+     (format "HSL City bikes data in database %s !" (:name db-name)))))
