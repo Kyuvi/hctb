9e5181000b87b258260e312049a734b79d3749a7
Kyuvi <Kyuvi@proton.me>
2023-02-03 20:13:47 +0200

    Corrected default data dir name in code core.clj

--- src/hctb/core.clj
+++ src/hctb/core.clj
@@ -11,9 +11,9 @@
   ([] (-main hs/default-db ))
   ([db]
    (let [csvdir (or (System/getenv "CSVDIR") default-csv-dir)]
-     (when  (= csvdir default-data-directory)
+     (when  (= csvdir default-csv-dir)
        (println (str "WARNING: No CSVDIR given as an environment variable,\n"
-                     "using default directory." default-data-directory ".")))
+                     "using default directory." default-csv-dir ".")))
      (when-not (hs/db-connection? db)
       (throw (Exception. (str "Unable to connect to DB:" db))))
      (hs/load-csvs-to-db db csvdir)
